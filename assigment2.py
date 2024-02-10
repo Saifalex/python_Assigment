e931acaf0139323a3f9d8b5a6cb51d6a33b54d6a
Saif <shaik.saif2468@gamil.com>
2024-02-10 20:06:32 +0530

    added assigments

--- assigment2.py
+++ assigment2.py
@@ -1,12 +1,12 @@
-# # 1.Write program to calculate the area of a rectangle given its length and width.
+# # # 1.Write program to calculate the area of a rectangle given its length and width.
 
 length = int(input("Please enter length rectangle: "))  # length of rectangle
 width = int(input("Please enter width of rectangle: "))  # width of rectangle
 
 area = width * length
 print(f"Area of rectangle is: {area} ")  # area of rectangle
-
-#2. Create a program that takes a user's name and age as input and prints a greeting message.
+#
+# #2. Create a program that takes a user's name and age as input and prints a greeting message.
 
 name = input("Please enter your name: ")
 age = int(input(f"Hi {name}, please enter your age: "))
@@ -18,27 +18,38 @@ elif age < 18:
     print(f"sorry! {name}, you have to wait for {remaining_years} more years to vote.")
 
 
-# #3 Given number is even or odd.
+# # #3 Given number is even or odd.
 
-userinput = int(input("Enter a number : "))
+userinput = int(input("Enter a number to check ever or odd? : "))
 
 if userinput % 2 == 0:
     print("it is an even number")
 else:
     print("it is an odd number")
 
-# Given a list of numbers, find a maximum and minimum values
-
+# # Given a list of numbers, find a maximum and minimum values
+#
 list = []
 listlength = int(input("Enter the range of list : "))
 
 for i in range(0,listlength):
-    usrinput = int(input(F"Enter the {i} value of list: "))
+    usrinput = int(input(F"Enter the {i} value of list : "))
     list.append(usrinput)
 
 print(f"list = {list}")
 print(f"maximum value of list : {max(list)}")
 print(f"minimum value of list : {min(list)}")
+
+# 5. Create a function to check if a given string is a palindrome
+userletter = input("Enter a palindrome word: ")
+letter = ""
+for i in userletter:
+    letter = i + letter
+print(letter)
+if userletter == letter:
+    print(userletter == letter)
+else:
+    print(userletter == letter)
 
 
 
