7dcf82509b67d81e056b4ae616c01863cae40f4e
Saif <shaik.saif2468@gamil.com>
2024-02-12 21:44:20 +0530

    added assigments

--- assigment2.py
+++ assigment2.py
@@ -1,12 +1,12 @@
-# # # 1.Write program to calculate the area of a rectangle given its length and width.
+# 1.Write program to calculate the area of a rectangle given its length and width.
 
 length = int(input("Please enter length rectangle: "))  # length of rectangle
 width = int(input("Please enter width of rectangle: "))  # width of rectangle
 
 area = width * length
 print(f"Area of rectangle is: {area} ")  # area of rectangle
-#
-# #2. Create a program that takes a user's name and age as input and prints a greeting message.
+
+#2. Create a program that takes a user's name and age as input and prints a greeting message.
 
 name = input("Please enter your name: ")
 age = int(input(f"Hi {name}, please enter your age: "))
@@ -18,7 +18,7 @@ elif age < 18:
     print(f"sorry! {name}, you have to wait for {remaining_years} more years to vote.")
 
 
-# # #3 Given number is even or odd.
+#3 Given number is even or odd.
 
 userinput = int(input("Enter a number to check ever or odd? : "))
 
@@ -27,8 +27,8 @@ if userinput % 2 == 0:
 else:
     print("it is an odd number")
 
-# # Given a list of numbers, find a maximum and minimum values
-#
+# Given a list of numbers, find a maximum and minimum values
+
 list = []
 listlength = int(input("Enter the range of list : "))
 
@@ -40,7 +40,7 @@ print(f"list = {list}")
 print(f"maximum value of list : {max(list)}")
 print(f"minimum value of list : {min(list)}")
 
-# 5. Create a function to check if a given string is a palindrome
+#5. Create a function to check if a given string is a palindrome
 userletter = input("Enter a palindrome word: ")
 letter = ""
 for i in userletter:
@@ -51,6 +51,15 @@ if userletter == letter:
 else:
     print(userletter == letter)
 
+# how to calculate given number of days into years, weeks and days?
+
+days = int(input("Enter the no days: "))
+
+years = days // 365
+remaining_days = days % 365
+weeks = remaining_days // 7
+remaining_days = remaining_days % 7
 
 
+print(f"given {days} days is equal to {years} years {weeks} weeks {remaining_days} days")
 
